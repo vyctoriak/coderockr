122c870297ce78295e1574e6ecb7c9f5efc66fae
vyctoria karina <vyctoriak@gmail.com>
2022-10-18 19:32:43 +0200

    article component iteration in home screen, sanitizing api texts and css refactor

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,40 +1,19 @@
 // css
-import { Airplay, CaretDoubleRight } from 'phosphor-react';
+import { CaretDoubleRight } from 'phosphor-react';
 import './Articles.scss';
 
-export default function Articles() {
+export default function Articles(props) {
   return (
-    <div className="articles">
-      <div className="articles__two--colums">
-        <div className="articles__item">
-          <div className="articles__photo">
-            <Airplay size={320} />
-          </div>
-          <div className="articles__info">
-            <span className="articles__author">Author</span>
-            <h1 className="articles__title">Title</h1>
-            <p className="articles__content">
-              Saepe quia culpa vero. Velit numquam corporis nihil sint enim
-              exercitationem. Rem nulla illum sint et id dolore voluptas
-            </p>
-          </div>
-          <CaretDoubleRight className="articles__icon" size={24} />
-        </div>
-        <div className="articles__item">
-          <div className="articles__photo">
-            <Airplay size={320} />
-          </div>
-          <div className="articles__info">
-            <span className="articles__author">Author</span>
-            <h1 className="articles__title">Title</h1>
-            <p className="articles__content">
-              Saepe quia culpa vero. Velit numquam corporis nihil sint enim
-              exercitationem. Rem nulla illum sint et id dolore voluptas
-            </p>
-          </div>
-          <CaretDoubleRight className="articles__icon" size={24} />
-        </div>
+    <div className="article__item">
+      <div className="article__photo">
+        <img src={props.imageUrl} />
       </div>
+      <div className="article__info">
+        <span>{props.author}</span>
+        <h3>{props.title}</h3>
+        <p>{props.article}</p>
+      </div>
+      <CaretDoubleRight className="article__icon" size={32} />
     </div>
   );
 }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,20 +1,41 @@
 import NavBar from '../components/NavBar/NavBar';
 import Articles from '../components/Articles/Articles';
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  // const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState([]);
 
-  // useEffect(() => {
-  //   fetch(`https://stormy-shelf-93141.herokuapp.com/articles`)
-  //     .then((response) => response.json())
-  //     .then((data) => setArticles(data));
-  // }, []);
+  useEffect(() => {
+    fetch(`https://stormy-shelf-93141.herokuapp.com/articles?_limit=3`)
+      .then((response) => response.json())
+      .then((data) => setArticles(data));
+  }, []);
+
+  function sanitize(text) {
+    return text
+      .trim()
+      .replace(/\n/g, '')
+      .replace(/\<p\>/g, '')
+      .replace(/\<\/p\>/g, '')
+      .replace(/\<img.*.\/\>/g, '');
+  }
 
   return (
-    <div>
+    <div className="home">
       <NavBar />
-      <Articles />
+      <div className="articles__container">
+        {articles.map((article) => {
+          return (
+            <Articles
+              key={article.id}
+              imageUrl={article.imageUrl}
+              author={article.author}
+              title={sanitize(article.title)}
+              article={sanitize(article.article)}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
