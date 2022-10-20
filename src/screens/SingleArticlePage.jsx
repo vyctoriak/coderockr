d71a1cb2527335f09c27f5ac705d8cf9a46ba285
vyctoria karina <vyctoriak@gmail.com>
2022-10-20 21:37:33 +0200

    broke single article page in components

--- src/screens/SingleArticlePage.jsx
+++ src/screens/SingleArticlePage.jsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
-// import Details from '../components/Details/Details';
-import './SingleArticlePage.scss';
 import { api } from '../api/axios';
 import { useParams } from 'react-router-dom';
-import SanitizeText from '../utils/SanitizeText';
+import Details from '../components/Details/Details';
 
 const SingleArticlePage = () => {
   let { id } = useParams();
@@ -23,26 +21,7 @@ const SingleArticlePage = () => {
     }
   };
 
-  return (
-    <div className="details__container">
-      {article && (
-        <div className="details__item">
-          <div className="details__info">
-            <div className="details__photo">
-              <img src={article.imageUrl} alt={article.title} />
-            </div>
-            <div className="details__titles">
-              <span>{article.date}</span>
-
-              <h3>{article.author}</h3>
-              <h2>{article.title}</h2>
-            </div>
-          </div>
-          <div className="details__text">{SanitizeText(article.article)}</div>
-        </div>
-      )}
-    </div>
-  );
+  return article && <Details {...article} />;
 };
 
 export default SingleArticlePage;
