c573885f05db394be388672a091be4aee0f2f3cd
vyctoria karina <vyctoriak@gmail.com>
2022-10-21 23:24:15 +0200

    renaming component

---
+++ src/components/ArticleDetail/ArticleDetail.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import SanitizeText from '../../utils/SanitizeText';
+import './ArticleDetail.scss';
+
+const ArticleDetail = ({ imageUrl, title, date, author, article }) => {
+  return (
+    <div className="details__container">
+      <div className="details__item">
+        <div className="details__info">
+          <div className="details__photo">
+            <img src={imageUrl} alt={title} />
+          </div>
+          <div className="details__titles">
+            <span>{date}</span>
+
+            <h3>{author}</h3>
+            <h2>{title}</h2>
+          </div>
+        </div>
+        <div className="details__text">{SanitizeText(article)}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ArticleDetail;
